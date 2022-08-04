4b7b5d82113c8c8c8d400532d786a9dcfbbd9f4f
imindMan <khanhdonglethanhtong7b2@gmail.com>
2022-08-04 20:52:03 +0700

    Add some libraries built on top of python

---
+++ python_library/better_list/main.py
@@ -0,0 +1,2 @@
+def append(value):
+    return value[0].value.append(value[1])
---
+++ python_library/ope_sys/main.py
@@ -0,0 +1,34 @@
+import os
+
+
+def name():
+    return os.name
+
+
+def command(value):
+    os.system(value[0].value)
+
+
+def create_a_new_dir(value):
+    os.makedirs(value[0].value, exist_ok=True)
+
+
+def create_a_new_file(value):
+    f = open(value[0].value, "r")
+    f.close()
+
+
+def list_files():
+    if os.name == "nt":
+        os.system("dir")
+
+    else:
+        os.system("ls")
+
+
+def trace_fullpath(value):
+    return os.path.abspath(value[0].value)
+
+
+def exist(value):
+    return os.path.exists(value[0].value)
