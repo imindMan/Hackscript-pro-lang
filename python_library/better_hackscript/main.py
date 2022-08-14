53f0a3b7df53ebe7f696793d51bda1907595041d
imindMan <khanhdonglethanhtong7b2@gmail.com>
2022-08-14 22:13:30 +0700

    Fix the rule again

--- python_library/better_hackscript/main.py
+++ python_library/better_hackscript/main.py
@@ -16,7 +16,7 @@ Rules of HackScript: (or Theory of HackScript)
 #7: No shorter code, instead, deal with nested thing and long code
 #8: Everything has to be organized, meaning it should be wrapped in parentheses
 #9: Everything, as the name of \"Hack\", is a challenge in HackScript, meaning error maybe doesn't show to you 
-#10: The purpose of HackScript is to be a fun programming language with no way escape (like estoric), but also 
+#10: The purpose of HackScript is to be a fun programming language with no way escape (like esotoric), but also 
     support high-level features to make it useful at some points.
 Happy coding!""")
 
