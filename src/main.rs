18781982446a63b8b51eaf57ad10a2b21b9294cf
imindMan <khanhdonglethanhtong7b2@gmail.com>
2023-02-12 20:55:35 +0700

    Commit first code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+// This is the Hackscript rebuild project written by imindMan
+// Due to slow speed of Hackscript in Python, Hackscript will be recreated again.
+//
+// Author: imindMan 
+// Date: 12-02-2023 08:35 PM (GMT +7)
+// Description: all the description you can find in the official repo
+// https://github.com/imindMan/Hackscript-pro-lang
+//
+// Rebuild in Rust
+
+
+// main file hackscript 
+
+use std::io::{self, Write};
+
+// input a command then run Hackscript 
+
+fn main() 
+{
+
+    loop {
+        // create a string to store command 
+        let mut command = String::new();
+    
+        print!("hackscript>");
+    
+        // input a command
+        let _ = io::stdout().flush();
+        io::stdin().read_line(&mut command).expect("Error reading from STDIN");
+        let output = run(command);
+        print!("{}", output);
+    }
+}
+
+// run the command 
+fn run(command: String) -> String 
+{
+    // for now, let's just return the string itself 
+    command
 }
