817e75ebb060119bd726b5beb1164f9c2f85f573
imindMan <khanhdonglethanhtong7b2@gmail.com>
2024-07-21 15:36:29 +0700

    Add some tests, found an error. In stage of fixing

---
+++ tests/string.rs
@@ -0,0 +1,143 @@
+use hackscript::run;
+
+// error checking for the string first
+
+#[test]
+fn endless_string_error() -> Result<(), String> {
+    match run(String::from("\"add")) {
+        Ok(_ok) => Err("Shouldn't be passed".to_string()),
+        Err(_err) => Ok(()),
+    }
+}
+
+#[test]
+fn trailing_character_error() -> Result<(), String> {
+    match run(String::from("\"\\a\"")) {
+        Ok(_ok) => Err("Shouldn't be passed".to_string()),
+        Err(_err) => Ok(()),
+    }
+}
+
+#[test]
+fn wrong_operation_subtract_error() -> Result<(), String> {
+    match run(String::from("\"string\" - \"str\"")) {
+        Ok(_ok) => Err("Shouldn't be passed".to_string()),
+        Err(_err) => Ok(()),
+    }
+}
+
+#[test]
+fn wrong_operation_multiply_error() -> Result<(), String> {
+    match run(String::from("\"string\" * \"str\"")) {
+        Ok(_ok) => Err("Shouldn't be passed".to_string()),
+        Err(_err) => Ok(()),
+    }
+}
+
+#[test]
+fn wrong_operation_divide_error() -> Result<(), String> {
+    match run(String::from("\"string\" / \"str\"")) {
+        Ok(_ok) => Err("Shouldn't be passed".to_string()),
+        Err(_err) => Ok(()),
+    }
+}
+
+// check the operation of string is working
+
+#[test]
+fn plus_operation() {
+    let run_res: String = match run(String::from("\"string\" + \"string\"")) {
+        Ok(ok) => format!("{}", ok),
+        Err(err) => format!("{}", err),
+    };
+
+    assert_eq!("stringstring\n".to_string(), run_res);
+}
+#[test]
+fn multiply_operation() {
+    let run_res: String = match run(String::from("\"string\" * 3")) {
+        Ok(ok) => format!("{}", ok),
+        Err(err) => format!("{}", err),
+    };
+
+    assert_eq!("stringstringstring\n".to_string(), run_res);
+}
+
+#[test]
+fn plus_complicated_operation() {
+    let run_res: String = match run(String::from("\"string\" + \"hi\" + \"there\"")) {
+        Ok(ok) => format!("{}", ok),
+        Err(err) => format!("{}", err),
+    };
+
+    assert_eq!("stringhithere\n".to_string(), run_res);
+}
+
+#[test]
+fn multiply_complicated_operation() {
+    let run_res: String = match run(String::from("\"string\" * 3 * 2")) {
+        Ok(ok) => format!("{}", ok),
+        Err(err) => format!("{}", err),
+    };
+
+    assert_eq!(
+        "stringstringstringstringstringstring\n".to_string(),
+        run_res
+    );
+}
+#[test]
+fn plus_multiply_complicated_operation() {
+    let run_res: String = match run(String::from("\"string\" * 3 * 2 + \"hi\"")) {
+        Ok(ok) => format!("{}", ok),
+        Err(err) => format!("{}", err),
+    };
+
+    assert_eq!(
+        "stringstringstringstringstringstringhi\n".to_string(),
+        run_res
+    );
+}
+
+#[test]
+fn plus_parentheses_operation() {
+    let run_res: String = match run(String::from("\"string\" + \"hi\" + (\"there\" + \"hey\")")) {
+        Ok(ok) => format!("{}", ok),
+        Err(err) => format!("{}", err),
+    };
+
+    assert_eq!("stringhitherehey\n".to_string(), run_res);
+}
+
+#[test]
+
+fn plus_parentheses_operation_combined_multiply() {
+    let run_res: String = match run(String::from(
+        "\"string\" + \"hi\" + (\"there\" * 3+ \"hey\")",
+    )) {
+        Ok(ok) => format!("{}", ok),
+        Err(err) => format!("{}", err),
+    };
+
+    assert_eq!("stringhitheretheretherehey\n".to_string(), run_res);
+}
+
+#[test]
+
+fn multiply_parentheses_operation() {
+    let run_res: String = match run(String::from(" \"string\" * (3 - 2*1)")) {
+        Ok(ok) => format!("{}", ok),
+        Err(err) => format!("{}", err),
+    };
+
+    assert_eq!("string\n".to_string(), run_res);
+}
+
+#[test]
+fn real_case_possible() {
+    let run_res: String = match run(String::from("\"\\t\\t\\tSTRING IMPLEMENTATION\" + \"\\n\\n\" + \"After hard work, it's finally here\"+ \"\\n\\n\" + \"Now, let's \\\"get into it\\\"\"") ) {
+        Ok(ok) => format!("{}", ok), 
+        Err(err) => format!("{}", err),
+    };
+
+    assert_eq!("\t\t\tSTRING IMPLEMENTATION\n\nAfter hard work, it's finally here\n\nNow, let's \"get into it\"\n", run_res);
+}
